4b710aa5eb776ec80cf0d4d3d499388d6de92784
Alexander Breuer <breuer@mytum.de>
2013-12-03 21:48:33 +0100

    Fixed naming of variables.

--- lectures/assembly/high_level.c
+++ lectures/assembly/high_level.c
@@ -20,8 +20,8 @@ void setInt( int *o_a, int *o_b, int *o_c ) {
   *o_c = 32;
 }
 
-void computeInt( int *o_a, int *o_b, int *o_c ) {
-  *o_c += (*o_a) * (*o_b);
+void computeInt( int *i_a, int *i_b, int *io_c ) {
+  *io_c += (i_a) * (*i_b);
 }
 
 void setDouble( double *o_x, double *o_y, double *o_z ) {
@@ -30,6 +30,6 @@ void setDouble( double *o_x, double *o_y, double *o_z ) {
   *o_z = 3.0;
 }
 
-void computeDouble( double *o_x, double *o_y, double *o_z ) {
-  *o_z += (*o_x) * (*o_y);
+void computeDouble( double *i_x, double *i_y, double *io_z ) {
+  *io_z += (*i_x) * (*i_y);
 }
